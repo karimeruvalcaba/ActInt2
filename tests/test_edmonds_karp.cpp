7ba8a43cb9dd44505d5d35fa6011cb3a15a94a71
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-02-02 19:20:51 +0000

    adding CmakeLists.txt

---
+++ tests/test_edmonds_karp.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "../edmonds_karp.h" 
+
+
+TEST(test_edmonds_karp, test_1_edmonds_karp)
+{
+    edmonds_karp edmonds_karp_test_1;
+    vector<vector<int>> capacity = {{0, 3, 3, 0, 0, 0},
+                                    {0, 0, 2, 3, 0, 0},
+                                    {0, 0, 0, 0, 2, 0},
+                                    {0, 0, 0, 0, 4, 2},
+                                    {0, 0, 0, 0, 0, 2},
+                                    {0, 0, 0, 0, 0, 3}};
+    
+    int V = capacity.size()
+    ASSERT_EQ(edmonds_karp.solve(V, capacity, 0, V - 1), 5);
+}
+
