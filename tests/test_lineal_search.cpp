31b9d3cb447b96610b66aea535b25f81e7597277
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-02-04 02:10:16 +0000

    tsp and lineal search tests done

---
+++ tests/test_lineal_search.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include "../lineal_search.h"
+
+TEST(test_lineal_search, test_1_lineal_search)
+{
+    vector<pair<int, int>> centrals = {{0, 0}, {5, 5}, {10, 10}};
+    pair<int, int> new_contract = {3, 3};
+    ASSERT_EQ(lineal_search::find(centrals, new_contract), make_pair(0, 0));
+}
+
+TEST(test_lineal_search, test_2_lineal_search)
+{
+    vector<pair<int, int>> centrals = {{1, 2}, {3, 4}, {6, 8}};
+    pair<int, int> new_contract = {4, 5};
+    ASSERT_EQ(lineal_search::find(centrals, new_contract), make_pair(3, 4));
+}
+
+TEST(test_lineal_search, test_3_lineal_search)
+{
+    vector<pair<int, int>> centrals = {{-5, -5}, {0, 0}, {5, 5}};
+    pair<int, int> new_contract = {-3, -3};
+    ASSERT_EQ(lineal_search::find(centrals, new_contract), make_pair(-5, -5));
+}
+
+TEST(test_lineal_search, test_4_lineal_search)
+{
+    vector<pair<int, int>> centrals = {{100, 100}, {200, 200}, {300, 300}};
+    pair<int, int> new_contract = {250, 250};
+    ASSERT_EQ(lineal_search::find(centrals, new_contract), make_pair(200, 200));
---
+++ tests/test_tsp.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "../tsp.h"
+
+// TEST 1: Grafo pequeño
+TEST(test_tsp, test_1_tsp)
+{
+    vector<vector<int>> distancias = {
+        {0, 10, 15, 20},
+        {10, 0, 35, 25},
+        {15, 35, 0, 30},
+        {20, 25, 30, 0}
+    };
+    vector<int> expected_route = {0, 1, 3, 2}; 
+    ASSERT_EQ(TSP::solve(4, distancias), expected_route);
+}
+
+// TEST 2: Grafo mediano 
+TEST(test_tsp, test_2_tsp)
+{
+    vector<vector<int>> distancias = {
+        {0, 29, 20, 21},
+        {29, 0, 15, 17},
+        {20, 15, 0, 28},
+        {21, 17, 28, 0}
+    };
+    vector<int> expected_route = {0, 2, 1, 3}; // One of the possible optimal routes
+    ASSERT_EQ(TSP::solve(4, distancias), expected_route);
+}
+
+// TEST 3: Grafo grande
+TEST(test_tsp, test_3_tsp
+{
+    vector<vector<int>> distancias = {
+        {0, 10, 8, 9, 7},
+        {10, 0, 10, 5, 6},
+        {8, 10, 0, 8, 9},
+        {9, 5, 8, 0, 6},
+        {7, 6, 9, 6, 0}
+    };
+    vector<int> expected_route = {0, 4, 1, 3, 2}; 
+    ASSERT_EQ(TSP::solve(5, distancias), expected_route);
+}
+
+// TEST 4: Solo una ruta valida
+TEST(test_tsp, test_4_tsp)
+{
+    vector<vector<int>> distancias = {
+        {0, 1},
+        {1, 0}
+    };
+    vector<int> expected_route = {0, 1};
+    ASSERT_EQ(TSP::solve(2, distancias), expected_route);
+}
