7be592ce2417ff8c91cb96bc8690f92c4f5a8378
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-02-04 03:27:12 +0000

    some edmonds_karp issues fixed

--- edmonds_karp.h
+++ edmonds_karp.h
@@ -1,24 +1,3 @@
-
-/*
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2013 SonarSource
- * http://github.com/SonarOpenCommunity/sonar-cxx
- *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 #ifndef EDMONDS_KARP_H
 #define EDMONDS_KARP_H
 
@@ -27,22 +6,44 @@
 #include <climits>
 using namespace std;
 
+/**
+ * @class EdmondsKarp
+ * @brief Implementación del algoritmo de Edmonds-Karp para encontrar el flujo máximo en una red de flujo.
+ */
 class EdmondsKarp {
 public:
+    /**
+     * @brief Calcula el flujo máximo en la red usando el algoritmo de Edmonds-Karp.
+     * @param n Número de nodos en el grafo.
+     * @param capacity Matriz de capacidades donde capacity[i][j] representa la capacidad del arco de i a j.
+     * @param source Nodo fuente.
+     * @param sink Nodo sumidero.
+     * @return El valor del flujo máximo de source a sink.
+     */
     static int solve(int n, const vector<vector<int>>& capacity, int source, int sink) {
         vector<vector<int>> flow(n, vector<int>(n, 0));
         vector<int> parent(n, -1);
         int max_flow = 0;
 
-        while (bfs(n, capacity, flow, source, sink, parent)) {
-            int flow_path = augmentFlow(capacity, flow, source, sink, parent);
+        while (BFS(n, capacity, flow, source, sink, parent)) {
+            int flow_path = AugmentFlow(capacity, flow, source, sink, parent);
             max_flow += flow_path;
         }
         return max_flow;
     }
 
 private:
-    static bool bfs(int n, const vector<vector<int>>& capacity, const vector<vector<int>>& flow,
+    /**
+     * @brief Realiza una búsqueda en anchura (BFS) para encontrar un camino aumentante en el grafo residual.
+     * @param n Número de nodos en el grafo.
+     * @param capacity Matriz de capacidades.
+     * @param flow Matriz de flujo actual.
+     * @param source Nodo fuente.
+     * @param sink Nodo sumidero.
+     * @param parent Vector donde se almacenará el camino encontrado.
+     * @return Verdadero si se encuentra un camino aumentante, falso en caso contrario.
+     */
+    static bool BFS(int n, const vector<vector<int>>& capacity, const vector<vector<int>>& flow,
                     int source, int sink, vector<int>& parent) {
         fill(parent.begin(), parent.end(), -1);
         queue<int> q;
@@ -57,17 +58,26 @@ private:
                 if (parent[v] == -1 && capacity[u][v] > flow[u][v]) {
                     parent[v] = u;
                     q.push(v);
+
                     if (v == sink) {
                         return true;
                     }
-
                 }
             }
         }
         return false;
     }
 
-    static int augmentFlow(const vector<vector<int>>& capacity, vector<vector<int>>& flow,
+    /**
+     * @brief Actualiza el flujo en el camino aumentante encontrado por BFS.
+     * @param capacity Matriz de capacidades.
+     * @param flow Matriz de flujo actual.
+     * @param source Nodo fuente.
+     * @param sink Nodo sumidero.
+     * @param parent Vector que contiene el camino aumentante.
+     * @return La cantidad de flujo añadida a la red.
+     */
+    static int AugmentFlow(const vector<vector<int>>& capacity, vector<vector<int>>& flow,
                            int source, int sink, const vector<int>& parent) {
         int flow_path = INT_MAX;
         for (int v = sink; v != source; v = parent[v]) {
